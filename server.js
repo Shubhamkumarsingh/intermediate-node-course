495d424f979714f47394616b32c235eb07237dc4
unknown <ss9337373@gmail.com>
2020-08-01 23:02:40 +0530

    refractor

--- server.js
+++ server.js
@@ -11,65 +11,15 @@ app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
 
-// CREATE
-app.post('/users',(req,res)=>{
- User.create({
-name:req.body.newData.name,
-email:req.body.newData.email,
-password:req.body.newData.password
-},
-(err,data)=>{
-if(err){
-res.json({success: false,message: err})
-}else if(!data){
-res.json({success: false,message: "Not found"})
-}else {
-res.json({success: true,data: data})
-}
-})
-})
-
-app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  User.findById(req.params.id,(err,data)=>{
-if(err){
-res.json({
-success:false,
-message: err
-})
-}else if(!data){
-res.json({
-success:false,
-message: "Not Found"
-})
-}else {
-res.json({
-success: true,
-data: data
-})
-}
-})
-})
-// UPDATE
-.put((req,res)=>{
-   User.findByIdAndUpdate(
-req.params.id,
-{
-name:req.body.newData.name,
-email:req.body.newData.email,
-password:req.body.newData.password
-},
-{
-new:true
-},
-(err,data)=>{
+//sendresponse
+function sendResponse(res,err,data){
 if(err){
 res.json({
 success:false,
 message:err
 })
-}else if(!data){
+}else if(!data)
+{
 res.json({
 success:false,
 message:"Not Found"
@@ -77,33 +27,36 @@ message:"Not Found"
 }else {
 res.json({
 success:true,
-data:data
+message:data
 })
 }
-}
+}	
+
+// CREATE
+app.post('/users',(req,res)=>{
+ User.create({...req.body.newData},
+(err,data)=>{sendResponse(res,err,data)}
 )
+})
+
+app.route('/users/:id')
+// READ
+.get((req,res)=>{
+  User.findById(req.params.id,(err,data)=>{sendResponse(res,err,data)})
+})
+// UPDATE
+.put((req,res)=>{
+   User.findByIdAndUpdate(
+req.params.id,
+{...req.param.newData},
+{
+new:true
+},
+(err,data)=>{sendResponse(res,err,data)})
 })
 // DELETE
 .delete((req,res)=>{
    User.findByIdAndDelete(
 req.params.id,
-(err,data)=>{
-if(err){
-res.json({
-success: false,
-message: err
-})
-}else if(!data){
-res.json({
-success:false,
-message:"Not Found"
-})
-}else{
-res.json({
-success:true,
-data:data
-})
-}
-}
-)
+(err,data)=>{sendResponse(res,err,data)})
 })
